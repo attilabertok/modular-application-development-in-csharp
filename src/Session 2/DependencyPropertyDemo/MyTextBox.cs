237356a6064f86e605dc188c892b884b39124416
bat <hun.azazel@gmail.com>
2023-04-25 12:52:15 +0200

    Dependency property demo

---
+++ src/Session 2/DependencyPropertyDemo/MyTextBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DependencyPropertyDemo
+{
+    public class MyTextBox : TextBox
+    {
+        public static readonly DependencyProperty DefaultTextProperty;
+        public static readonly string DefaultTextPropertyName = "DefaultText";
+        public string DefaultText
+        {
+            get => (string)GetValue(DefaultTextProperty);
+            set => SetValue(DefaultTextProperty, value);
+        }
+
+        static MyTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyTextBox), new FrameworkPropertyMetadata(typeof(MyTextBox)));
+            MyTextBox.DefaultTextProperty = DependencyProperty.Register(
+                DefaultTextPropertyName,
+                typeof(string),
+                typeof(MyTextBox),
+                new FrameworkPropertyMetadata(string.Empty));
+        }
+    }
+}
